0498d8e6d9577b6f0f1c14313cf5dd570d540282
Andrew Furth <andrew.furth@gmail.com>
2016-06-25 15:40:23 -0700

    basic text and icons added to home; portfolio jquery established

--- js/script.js
+++ js/script.js
@@ -4,3 +4,43 @@ $(document).ready(function() {
       anchors: ["home", "portfolio", "about"]
     });
 });
+
+//  Add projects to the page
+var github = 'https://github.com/afurth89/'
+var projects = [
+  { name: 'What Country Am I?', url: 'http://andrew-furth-geography-game.bitballoon.com/',
+    code: github + 'WhatCountryAmI' },
+  { name: 'Linguo', url: 'https://linguoisdead.herokuapp.com/',
+    code: github + 'linguo' },
+];
+
+var fragment = document.createDocumentFragment();
+
+projects.forEach(function(project) {
+  var div  = make("div"),
+      a    = make("a"),
+      code = make('a'),
+      h2   = make("h2"),
+      img  = make("img")
+
+  a.href = project.url;
+  h2.innerHTML = project.name.replace(/-/g, " ");
+  img.src = "images/" + project.name + ".png";
+  a.appendChild(h2);
+  a.appendChild(img);
+
+  code.className = 'code';
+  code.innerHTML = 'Source code';
+  code.href = project.code;
+
+  div.appendChild(a);
+  div.appendChild(code);
+  div.className = "project";
+  fragment.appendChild(div);
+});
+
+document.getElementById("sect-portfolio").appendChild(fragment);
+
+function make(tag) {
+  return document.createElement(tag);
+}
