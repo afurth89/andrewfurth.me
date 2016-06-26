bf1a9a820f2680d0fa8f14b8564c41662263dc43
Andrew Furth <andrew.furth@gmail.com>
2016-06-26 15:27:29 -0700

    added favicon to try and fix deployment error

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+
 var port = process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log('Server listening on port '+port+'...');
